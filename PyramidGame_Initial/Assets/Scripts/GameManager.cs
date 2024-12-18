f5251308501838e8e82e152b8e5dc0173b0d202c
msepse <114550628+msepse@users.noreply.github.com>
2024-12-18 09:55:04 -0500

    Win screen now triggers upon contact with chest, made all good tiles appear "bad" to make the solution not obvious (route remains the same)

--- PyramidGame_Initial/Assets/Scripts/GameManager.cs
+++ PyramidGame_Initial/Assets/Scripts/GameManager.cs
@@ -13,10 +13,12 @@ public class GameManager : MonoBehaviour
     public Transform playerTransform; // Reference to the player
     public Vector3 initialPlayerPosition; // Starting position for the player
 
-    public GameObject WinText;
+    public GameObject WinText; // Reference to the Win text object
     public GameObject gameOverText; // Reference to the Game Over text object
     public GameObject quitGameButton; // Reference to the Quit Game button object
     public GameObject playGameAgainButton; // Reference to the Play Game Again button object
+    public GameObject WinnerquitGameButton; // Reference to the Quit Game button object
+    public GameObject WinnerplayGameAgainButton; // Reference to the Play Game Again button object
 
     private void Start()
     {
@@ -24,10 +26,13 @@ public class GameManager : MonoBehaviour
         // Store the player's initial position
         initialPlayerPosition = playerTransform.position;
 
-        WinText.SetActive(false);
         gameOverText.SetActive(false); // Ensure Game Over text is initially hidden
         quitGameButton.SetActive(false); // Ensure Quit Game button is initially hidden
         playGameAgainButton.SetActive(false); // Ensure Play Game Again button is initially hidden
+
+        WinText.SetActive(false); // Ensure Win text is initially hidden
+        WinnerquitGameButton.SetActive(false); // Ensure Win Quit Game button is initially hidden
+        WinnerplayGameAgainButton.SetActive(false); // Ensure Win Play Game Again button is initially hidden
     }
 
     public void GameOver()
@@ -40,12 +45,13 @@ public class GameManager : MonoBehaviour
 
     }
 
-    public void Winner(){
-        Debug.Log("You Win");
+    public void Winner()
+    {
+        Debug.Log("You Win!");
 
-        WinText.SetActive(true);
-        quitGameButton.SetActive(true); // Show Quit Game button
-        playGameAgainButton.SetActive(true); // Show Play Game Again button
+        WinText.SetActive(true); // Show Win text
+        WinnerquitGameButton.SetActive(true); // Show Quit Game button
+        WinnerplayGameAgainButton.SetActive(true); // Show Play Game Again button
     }
     private void RestartGame()
     {
--- PyramidGame_Initial/Assets/Scripts/WinTrigger.cs
+++ PyramidGame_Initial/Assets/Scripts/WinTrigger.cs
@@ -1,22 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI; // Import the UI namespace
 using UnityEngine.SceneManagement; // Import SceneManagement namespace
+using TMPro; // For TextMeshPro
 
 public class WinTrigger : MonoBehaviour
 {
-    public Text winText; // Reference to the Text component
-    public Button quitButton; // Reference to the Quit Button
-    public Button playAgainButton; // Reference to the Play Again Button
+    //public GameObject winText; // Reference to the Text component
+    //public Button quitButton; // Reference to the Quit Button
+    //public Button playAgainButton; // Reference to the Play Again Button
 
-    private void Start()
-    {
-        winText.gameObject.SetActive(false); // Ensure the text is initially inactive
-        quitButton.gameObject.SetActive(false); // Ensure the button is initially inactive
-        playAgainButton.gameObject.SetActive(false); // Ensure the button is initially inactive
+    public GameManager gameManager; // Drag the GameManager here in the Inspector
 
-        quitButton.onClick.AddListener(QuitGame); // Add listener to Quit button
-        playAgainButton.onClick.AddListener(PlayAgain); // Add listener to Play Again button
-    }
+    //private void Start()
+    //{
+    //    winText.SetActive(false); // Ensure the text is initially inactive
+    //    quitButton.gameObject.SetActive(false); // Ensure the button is initially inactive
+    //    playAgainButton.gameObject.SetActive(false); // Ensure the button is initially inactive
+
+    //    quitButton.onClick.AddListener(QuitGame); // Add listener to Quit button
+    //    playAgainButton.onClick.AddListener(PlayAgain); // Add listener to Play Again button
+    //}
 
     private void OnTriggerEnter(Collider other)
     {
@@ -28,22 +33,24 @@ public class WinTrigger : MonoBehaviour
 
     private void ShowWinScreen()
     {
-        winText.gameObject.SetActive(true); // Activate the win screen text
-        quitButton.gameObject.SetActive(true); // Activate the Quit button
-        playAgainButton.gameObject.SetActive(true); // Activate the Play Again button
+        //winText.SetActive(true); // Activate the win screen text
+        //quitButton.gameObject.SetActive(true); // Activate the Quit button
+        //playAgainButton.gameObject.SetActive(true); // Activate the Play Again button
+
+        gameManager.Winner();
     }
 
-    private void QuitGame()
-    {
-        Application.Quit(); // Quit the application
-        #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false; // For use in the editor
-        #endif
-    }
+    //private void QuitGame()
+    //{
+    //    Application.Quit(); // Quit the application
+    //    #if UNITY_EDITOR
+    //    UnityEditor.EditorApplication.isPlaying = false; // For use in the editor
+    //    #endif
+    //}
 
-    private void PlayAgain()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Reload the current scene
-    }
+    //private void PlayAgain()
+    //{
+    //    SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Reload the current scene
+    //}
 }
 
